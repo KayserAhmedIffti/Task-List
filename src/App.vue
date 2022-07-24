2cafeb9056916971d5a64cfe6f11e722f6f5a054
Kayser Ahmed <kayser.ahmed@northsouth.edu>
2022-07-24 17:08:49 +0600

    Doctor's task tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggleAddTask="toggleAddTask" title="Task Tracker"
+    :showAddTask="showAddTask"
+        />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask"/>
+    </div>
+
+    <TasksComponent @toggle-reminder="toggleReminder"
+     @delete-task="deleteTask" :tasks="tasks"
+     /> 
+
+  </div>
+  <!-- :tasks is used in this component property-->
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from "./components/Header";
+  import TasksComponent from "./components/Tasks";
+  import AddTask from "./components/AddTask";
 
-export default {
+  export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    Header,
+    TasksComponent,
+    AddTask
+  },
+
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    }
+  },
+
+  methods:{
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task){
+      this.tasks = [...this.tasks,task]
+    },
+    
+    deleteTask(id) {
+      if(confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+
+    toggleReminder(id) {
+        this.tasks = this.tasks.map((task) =>
+          task.id === id 
+          ? {...task, reminder: !task.reminder} 
+          : task
+        )
+      },
+      
+  },
+
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Apoinment',
+        day: 'March 1st at 2.30 p.m',
+        reminder: true,
+      },
+       {
+        id: 2,
+        text: 'Meeting at University reunion',
+        day: 'April 15th at 5.30 p.m',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'August 20th at 9.30 p.m',
+        reminder: false,
+      },
+    ]
+   }
+  
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .container {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    margin-top: 60px;
+    border: 1px solid;
+  }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,51 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder',task.id)" 
+    :class="[task.reminder ? 'reminder' : 'red', 'task']">
+
+        <h3>
+        {{task.text}}
+        <i @click="$emit('delete-task',task.id)" class="fas fa-times"></i>
+        </h3>
+        
+        <p> {{task.day}} </p>
+
+    </div>
+</template>
+
+<script>
+export default 
+{
+    name:'TaskIn',
+    props:
+    {
+        task: Object,
+    },
+    
+}
+</script>
+
+<style scope>
+.fas{
+    color: red;
+}
+.task{
+    background: #e0e0e0;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+.task.reminder {
+    border-left: 5px solid green;
+}
+
+.task h3{
+display:flex;
+align-items: center;
+justify-content: space-between;
+}
+.red{
+border-left: 5px solid red;
+}
+
+
+</style>
